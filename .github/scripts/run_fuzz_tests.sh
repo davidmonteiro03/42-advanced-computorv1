cfa6fe1f4cb79b62528d8778593f9c6fa9240542
davidmonteiro03 <damonteiro12446@gmail.com>
2026-06-29 09:49:52 +0100

    testing actions

--- .github/scripts/run_fuzz_tests.sh
+++ .github/scripts/run_fuzz_tests.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-make
+
+test () {
+	if ! [ $# -eq 2 ]; then return; fi
+	input="$1"
+	correct="$2"
+	make
+	./computor "$input" > "executed.txt"
+	echo -n "$correct" > "correct.txt"
+	if ! diff "executed.txt" "correct.txt"; then exit 1; fi
+}
+
+test "" "Reduced form: 0 = 0
+Any real number is a solution.
+"
